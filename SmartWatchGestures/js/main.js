8942d432e9f26272103a0ef99b45d8ea63ed1a09
Pispa339 <pispa.juho.k@student.uta.fi>
2015-03-25 18:13:09 +0200

    index view and document view with early gesture scrolling functionality.

--- SmartWatchGestures/js/main.js
+++ SmartWatchGestures/js/main.js
@@ -1,5 +1,7 @@
 //testing testing
+
 window.onload = function () {
+	
     // TODO:: Do your initialization job
 
     // add eventListener for tizenhwkey
@@ -8,6 +10,8 @@ window.onload = function () {
             tizen.application.getCurrentApplication().exit();
     });
 
+    screen.lockOrientation("portrait");
+    
     // Sample code
     var textbox = document.querySelector('.contents');
     textbox.addEventListener("click", function(){
@@ -22,13 +26,50 @@ window.onload = function () {
     var accValueZ = document.querySelector('#accValueZ');
     
     window.addEventListener('devicemotion', function(e) {
-        var ax = e.accelerationIncludingGravity.x;
-        var ay = -e.accelerationIncludingGravity.y;
-        var az = -e.accelerationIncludingGravity.z;
-        accValueX.innerHTML = ax;
-        accValueY.innerHTML = ay;
-        accValueZ.innerHTML = az;
+        var ax = Math.floor(e.accelerationIncludingGravity.x);
+        var ay = Math.floor(-e.accelerationIncludingGravity.y);
+        var az = Math.floor(-e.accelerationIncludingGravity.z);
+        accValueX.innerHTML = "X:" + ax;
+        accValueY.innerHTML = "Y:" + ay;
+        accValueZ.innerHTML = "Z:" + az;
+        
+        if(ay > 1 && ay < 3){
+        	var newScrollPoint = window.pageYOffset - 4;
+        	window.scrollTo(0, newScrollPoint);
+        }
+        if(ay < -3 && ay > -6){
+        	var newScrollPoint = window.pageYOffset + 4;
+        	window.scrollTo(0, newScrollPoint);
+        }
+        if(ay > 2 && ay < 5){
+        	var newScrollPoint = window.pageYOffset - 9;
+        	window.scrollTo(0, newScrollPoint);
+        }
+        if(ay < -5 && ay > -8){
+        	var newScrollPoint = window.pageYOffset + 9;
+        	window.scrollTo(0, newScrollPoint);
+        }
+        if(ay > 4){
+        	var newScrollPoint = window.pageYOffset - 20;
+        	window.scrollTo(0, newScrollPoint);
+        }
+        if(ay < -7){
+        	var newScrollPoint = window.pageYOffset + 20;
+        	window.scrollTo(0, newScrollPoint);
+        }
+        setScreenBrightness(getScreenBrightness());
+        
+        
+        
+        var rotx = Math.floor(e.rotationRate.alpha);
+        var roty = Math.floor(e.rotationRate.beta);
+        var rotz = Math.floor(e.rotationRate.gamma);
  
+        rotValueX.innerHTML = "Alpha: " + rotx;
+        rotValueY.innerHTML = "Beta: " + roty;
+        rotValueZ.innerHTML = "Gamma: " + rotz;
     });
+    
+   
     
 };
